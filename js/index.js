a00b265b3f3acd2a6595f90f0569c93de1af86a7
SerhiiPerekhrest <110435711+SerhiiPerekhrest@users.noreply.github.com>
2022-08-21 01:41:23 +0300

    java

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const age = 10;
+const totalPric = 200.74;
+const userName = 'Chelsy';
+const message = 'Hello';
+const isOpen = true;
+const shouldContfirm = false;
+
+const type = typeof isOpen;
+console.log(type);
+console.log('do');
+alert('lkjlkj');
+console.log('after');
+
